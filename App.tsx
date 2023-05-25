d12c8f0d041762afb1896d65e42c763b7f51bb80
Lucie Chauvet <l.chauvet@beapp.fr>
2023-05-25 12:07:11 +0200

    feat: wip navigations

--- App.tsx
+++ App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MapScreen from './ui/screens/MapScreen';
-import StationsListScreen from './ui/screens/StationsListScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MapScreen from "./ui/screens/MapScreen";
+import StationsListScreen from "./ui/screens/StationsListScreen";
+import {
+  createStackNavigator,
+  StackNavigationProp,
+} from "@react-navigation/stack";
+import StationDetailsScreen from "./ui/screens/StationDetailsScreen";
 
 type RootTabParamList = {
-  MapScreen: undefined;
-  StationsList: undefined;
+  Carte: undefined;
+  Stations: undefined;
+};
+
+export type RootStackParamList = {
+  StationsListScreen: undefined;
+  StationDetailsScreen: { stationId: string };
 };
 
 const Tab = createBottomTabNavigator<RootTabParamList>();
+const Stack = createStackNavigator<RootStackParamList>();
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-      <Tab.Screen name="StationsList" component={StationsListScreen} />
-        <Tab.Screen name="MapScreen" component={MapScreen} />
+        <Tab.Screen name="Stations" component={StackScreen} />
+        <Tab.Screen name="Carte" component={MapScreen} />
       </Tab.Navigator>
     </NavigationContainer>
+  );
+};
+
+const StackScreen: React.FC = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="StationsListScreen"
+        component={StationsListScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="StationDetailsScreen"
+        component={StationDetailsScreen}
+      />
+    </Stack.Navigator>
   );
 };
 
